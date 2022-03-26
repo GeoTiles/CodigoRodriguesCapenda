403ec9b2eb0fd20dd566e54936d0dd54d847becb
capendaJunior <67684496+capendaJunior@users.noreply.github.com>
2022-03-26 10:50:05 +0100

    Add files via upload
    O métodos estão dos declarados com boolean. A minha ideia foi usar o retorno apenas para saber se os primos foram ou não encontrados, e caso tenham sido encontrados, retorná-los por referência.

--- factorizarPrimos.cpp
+++ factorizarPrimos.cpp
@@ -32,7 +32,7 @@ bool FactorizarPrimos::testaPrimo(mpz_t cand){
  */
 void  FactorizarPrimos::metodoEuclides(mpz_t n){
   mpz_t min,max,product,p,q;
-  mpz_inits(min,max,product,p,q,NULL);
+  mpz_inits(min,max,product,p,q);
   
   mpz_set_ui(min,3);
   mpz_sqrt(max,n);
@@ -47,9 +47,7 @@ void  FactorizarPrimos::metodoEuclides(mpz_t n){
     mpz_add_ui(min,min,1);
   }
   mpz_gcd (p,n,product);	
-  mpz_cdiv_q (q, n, p);
-
-  gmp_printf("%Zd = %Zd*%Zd\n",n,p,q);
+  mpz_cdiv_q (q, n, p);	
   
 }
 
